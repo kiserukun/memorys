1d1037ede4a43469e258a996cfd183bbb50a0784
kiserukun <73691576+kiserukun@users.noreply.github.com>
2020-12-19 22:24:11 +0900

    comi

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'memorys/index'
   root to: "memorys#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :memorys 
 end
